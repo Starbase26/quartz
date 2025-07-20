3b67a912bb3ec0489a96a2a352333f91957ffa77
Kevin Schaaps <github@ks.starbase26.nl>
2025-07-20 14:23:05 +0000

    Quartz sync: Jul 20, 2025, 2:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -27,7 +27,7 @@ const config: QuartzConfig = {
         // header: "Schibsted Grotesk",
         // header: "Merienda",
         // body: "Montserrat Alternates",
-        header: "Cormorant",
+        header: "IBM Plex Sans",
         body: "Newsreader",
         code: "Anonymous Pro",
       },
